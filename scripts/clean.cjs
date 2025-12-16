25f19b91390e29f8180c8808cb700afa00719a73
xskutsu <xsku@protonmail.com>
2025-12-16 14:18:44 -0600

    Clean script

---
+++ scripts/clean.cjs
@@ -0,0 +1,22 @@
+const fs = require("fs");
+const path = require("path");
+const projectRoot = path.resolve(__dirname, "..");
+const pathsToDelete = [
+	"dist/"
+];
+console.log("Starting cleaning...");
+for (let p of pathsToDelete) {
+	p = path.join(projectRoot, p);
+	try {
+		fs.rmSync(p, {
+			recursive: true
+		});
+	} catch (error) {
+		if (error.code === "ENOENT") {
+			console.error("Skipping missing path:", p);
+		} else {
+			console.error("Failed to delete path:", p, error);
+		}
+	}
+}
+console.log("Cleaning complete.");
