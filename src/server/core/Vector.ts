7564d666fc6b6e25f1aa3385e2c3d0be9bfb4f72
xskutsu <xsku@protonmail.com>
2025-12-19 20:58:19 -0600

    Add Vector2 type

--- src/server/core/Vector.ts
+++ src/server/core/Vector.ts
@@ -1,4 +1,6 @@
-export class Vector {
+import { Vector2 } from "../types/Vector2";
+
+export class Vector implements Vector2 {
 	public x: number;
 	public y: number;
 	constructor(x: number, y: number) {
